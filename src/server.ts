e67fecd3ba1e7205dda9f2c148f65fde488f2f5b
michaelperryjnr <michaelperryt97@gmail.com>
2025-11-05 16:43:38 +0000

    Fixed clean exit

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ import logger from "./lib/logger";
 const PORT = CONFIG.PORT;
 
 app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
