e9587de0071d2402e4a8143dbf77489347b8d5ed
michaelperryjnr <michaelperryt97@gmail.com>
2025-11-05 16:47:44 +0000

    Added exception handlers

--- src/server.ts
+++ src/server.ts
@@ -7,3 +7,14 @@ const PORT = CONFIG.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+console.log("✅ Server file loaded");
+process.on("exit", (code) =>
+  console.log("❌ Process exiting with code:", code)
+);
+process.on("uncaughtException", (err) =>
+  console.error("💥 Uncaught Exception:", err)
+);
+process.on("unhandledRejection", (reason) =>
+  console.error("💥 Unhandled Rejection:", reason)
+);
